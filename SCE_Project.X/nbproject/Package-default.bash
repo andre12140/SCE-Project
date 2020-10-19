b91b6e96cdd0cd431ab8f78931501146ef883093
andre12140 <andre_._barbosa@hotmail.com>
2020-10-19 23:52:10 +0100

    Changed project name

--- SCE_Project.X/nbproject/Package-default.bash
+++ SCE_Project.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/tstLCD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=tstLCD.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=tstlcd.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SCE_Project.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=SCE_Project.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=sceproject.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/tstlcd.x/bin
+makeDirectory ${TMPDIR}/sceproject.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/tstlcd.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/sceproject.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/tstlcd.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/sceproject.x.tar *
 checkReturnCode
 
 # Cleanup
